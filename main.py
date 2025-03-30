0b45fa5f179e2dcb931d586719e3253201607fa1
AhmedAbdLMoaty <63984192+AhmedAbdLMoaty@users.noreply.github.com>
2025-03-30 22:50:57 +0200

    Full FordFulkerson

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Graph:
 
         while queue:
             u = queue.pop(0)
-
+            print(u)
             for ind, val in enumerate(self.graph[u]):
                 if visited[ind] == False and val > 0:
 
@@ -30,9 +30,7 @@ class Graph:
         return False
 
     def FordFulkerson(self, source, sink):
-
         parent = [-1]*(self.ROW)
-
         max_flow = 0
 
         while self.BFS(source, sink, parent):
@@ -40,5 +38,28 @@ class Graph:
 
             s = sink
             while s != source:
-                path_flow = min(path_flow, self.graph[parent[s][s]])
+                path_flow = min(path_flow, self.graph[parent[s]][s])
                 s = parent[s]
+            max_flow += path_flow
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
+
+        
+graph = [[0, 16, 13, 0, 0, 0],
+        [0, 0, 10, 12, 0, 0],
+        [0, 4, 0, 0, 14, 0],
+        [0, 0, 9, 0, 0, 20],
+        [0, 0, 0, 7, 0, 4],
+        [0, 0, 0, 0, 0, 0]]
+g = Graph(graph)
+
+source = 0; sink = 5
+
+print ("The maximum possible flow is %d " % g.FordFulkerson(source, sink))
+
