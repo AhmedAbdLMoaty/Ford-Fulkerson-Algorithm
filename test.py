b877783ac71af3967d25be229b777a1470518c7d
AhmedAbdLMoaty <63984192+AhmedAbdLMoaty@users.noreply.github.com>
2025-03-30 22:51:57 +0200

    deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-a = ["Geeks", "for", "Geeks"]
-
-# Iterating list using enumerate to get both index and element
-for i, name in enumerate(a):
-    print(f"Index {i}: {name}")
-
-# Converting to a list of tuples
-print(list(enumerate(a)))
